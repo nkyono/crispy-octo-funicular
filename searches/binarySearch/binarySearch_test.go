84256f7ba275eccf9e6b3206e2d0164e5c854ea4
nkyono <38087905+nkyono@users.noreply.github.com>
2021-05-01 00:06:28 -0700

    Created binary search for range
    Need to also do better algorithm that results in O(log(n)) rather than O(n)

--- searches/binarySearch/binarySearch_test.go
+++ searches/binarySearch/binarySearch_test.go
@@ -5,6 +5,41 @@ import (
 	"testing"
 )
 
+func SpecialBinarySearch(arr []int, target int) []int {
+	high, low := len(arr)-1, 0
+	mid := (high + low) / 2
+
+	/* Actually a much better strategy is to use binary search on each side of the found index
+	 * If that search returned -1, we would know that it's the furthest most index
+	 * by doing multiple binary searches we would stay in an O(log(n)) range rather than the O(n) case here
+	 * worst case is that array only contains target so we would iterate through the whole array
+	 * binary would continuously divide in half leading to desired O(log(n)) behavior
+	 */
+	for low < high {
+		if target == arr[mid] {
+			i := 0
+			// works since mid-i is first check so guaranteed that mid-(i+1) will only happen when mid-i > 0
+			for mid-i > 0 && arr[mid-(i+1)] == target {
+				i++
+			}
+			j := 0
+			for mid+j < len(arr)-1 && arr[mid+(j+1)] == target {
+				j++
+			}
+			fmt.Printf("mid: %d, i: %d, j:%d\n", mid, i, j)
+			return []int{mid - i, mid + j}
+		}
+		if target > arr[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+		mid = (high + low) / 2
+	}
+
+	return []int{-1, -1}
+}
+
 func BinarySearch(arr []int, target int) int {
 	high, low := len(arr)-1, 0
 	mid := (high + low) / 2
@@ -24,6 +59,21 @@ func BinarySearch(arr []int, target int) int {
 	return -1
 }
 
+func compSlice(a, b []int) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestBinarySearch(t *testing.T) {
 	var tests = []struct {
 		inArray  []int
@@ -36,19 +86,39 @@ func TestBinarySearch(t *testing.T) {
 		{[]int{1, 2, 3, 4, 5, 6}, -1, -1},
 		{[]int{4, 5, 6}, 1, -1},
 		{[]int{}, 3, -1},
-		/*
-			{[]int{1, 2, 3, 4, 4, 5}, 4, 4}, // I don't think I necessarily want it to take in list with multiple of the same value
-			{[]int{1, 2, 2, 3, 4, 4, 5}, 2, 1},
-			{[]int{2, 2, 2, 2}, 2, 0},
-		*/
 	}
 
 	for _, tt := range tests {
-		testname := fmt.Sprintf("%v, %d", tt.inArray, tt.inTarget)
+		testname := fmt.Sprintf("Regular: %v, %d", tt.inArray, tt.inTarget)
 		t.Run(testname, func(t *testing.T) {
 			ans := BinarySearch(tt.inArray, tt.inTarget)
 			if ans != tt.want {
 				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+
+	var testsSpecial = []struct {
+		inArray  []int
+		inTarget int
+		want     []int
+	}{
+		{[]int{1, 2, 4, 4, 5, 6}, 4, []int{2, 3}},
+		{[]int{1, 1, 1, 2, 4, 5, 6}, 1, []int{0, 2}},
+		{[]int{1, 2, 4, 4, 4}, 4, []int{2, 4}},
+		{[]int{1, 2, 3, 4, 5, 6}, 9, []int{-1, -1}},
+		{[]int{1, 2, 3, 4, 5, 6}, -1, []int{-1, -1}},
+		{[]int{4, 5, 6}, 1, []int{-1, -1}},
+		{[]int{}, 3, []int{-1, -1}},
+		{[]int{1, 1, 1, 1}, 1, []int{0, 3}},
+	}
+
+	for _, tt := range testsSpecial {
+		testname := fmt.Sprintf("Special: %v, %d", tt.inArray, tt.inTarget)
+		t.Run(testname, func(t *testing.T) {
+			ans := SpecialBinarySearch(tt.inArray, tt.inTarget)
+			if !compSlice(ans, tt.want) {
+				t.Errorf("got %v, want %v", ans, tt.want)
 			}
 		})
 	}
