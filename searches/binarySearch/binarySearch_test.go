ced153bbb39b0c754247dcf69347ced51851604e
nkyono <38087905+nkyono@users.noreply.github.com>
2021-05-02 18:37:29 -0700

    Update binarySearch_test.go

--- searches/binarySearch/binarySearch_test.go
+++ searches/binarySearch/binarySearch_test.go
@@ -5,6 +5,53 @@ import (
 	"testing"
 )
 
+func RangeBinarySearch(arr []int, left, right, target int) int {
+	if left < 0 || right > len(arr) || len(arr) == 0 {
+		return -1
+	}
+	if left == right && arr[left] == target {
+		return left
+	}
+	// fmt.Printf("%d, %v, %d, %d\n", target, arr, left, right)
+	high, low := right, left
+	mid := (high + low) / 2
+
+	for low <= high {
+		// fmt.Printf("low: %d, mid: %d, high: %d, target: %d, arr[mid]: %d\n", low, mid, high, target, arr[mid])
+		if target == arr[mid] {
+			return mid
+		}
+		if target > arr[mid] {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+		mid = (high + low) / 2
+	}
+	return -1
+}
+
+func DoubleBinarySearch(arr []int, target int) []int {
+	ans := []int{-1, -1}
+	mid := BinarySearch(arr, target)
+	if mid != -1 {
+		temp := -1
+		left := mid
+		right := mid
+		for left != -1 {
+			temp = left
+			left = RangeBinarySearch(arr, 0, left-1, target)
+		}
+		ans[0] = temp
+		for right != -1 {
+			temp = right
+			right = RangeBinarySearch(arr, right+1, len(arr)-1, target)
+		}
+		ans[1] = temp
+	}
+	return ans
+}
+
 func SpecialBinarySearch(arr []int, target int) []int {
 	high, low := len(arr)-1, 0
 	mid := (high + low) / 2
@@ -26,7 +73,6 @@ func SpecialBinarySearch(arr []int, target int) []int {
 			for mid+j < len(arr)-1 && arr[mid+(j+1)] == target {
 				j++
 			}
-			fmt.Printf("mid: %d, i: %d, j:%d\n", mid, i, j)
 			return []int{mid - i, mid + j}
 		}
 		if target > arr[mid] {
@@ -98,6 +144,31 @@ func TestBinarySearch(t *testing.T) {
 		})
 	}
 
+	var testsRange = []struct {
+		inArray  []int
+		inTarget int
+		left     int
+		right    int
+		want     int
+	}{
+		{[]int{1, 2, 4, 5, 6}, 4, 0, 4, 2},
+		{[]int{1, 2, 4, 5, 6, 7}, 4, 0, 3, 2},
+		{[]int{1, 2, 4, 5, 6, 7}, 6, 4, 5, 4},
+		{[]int{1, 2, 3, 4, 5, 6}, 2, 2, 5, -1},
+		{[]int{4, 5, 6}, 1, 0, 2, -1},
+		{[]int{}, 3, 0, 0, -1},
+	}
+
+	for _, tt := range testsRange {
+		testname := fmt.Sprintf("Range: %v, %d", tt.inArray, tt.inTarget)
+		t.Run(testname, func(t *testing.T) {
+			ans := RangeBinarySearch(tt.inArray, tt.left, tt.right, tt.inTarget)
+			if ans != tt.want {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
+	}
+
 	var testsSpecial = []struct {
 		inArray  []int
 		inTarget int
@@ -117,6 +188,31 @@ func TestBinarySearch(t *testing.T) {
 		testname := fmt.Sprintf("Special: %v, %d", tt.inArray, tt.inTarget)
 		t.Run(testname, func(t *testing.T) {
 			ans := SpecialBinarySearch(tt.inArray, tt.inTarget)
+			if !compSlice(ans, tt.want) {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+		})
+	}
+
+	var testsDouble = []struct {
+		inArray  []int
+		inTarget int
+		want     []int
+	}{
+		{[]int{1, 2, 4, 4, 5, 6}, 4, []int{2, 3}},
+		{[]int{1, 1, 1, 2, 4, 5, 6}, 1, []int{0, 2}},
+		{[]int{1, 2, 4, 4, 4}, 4, []int{2, 4}},
+		{[]int{1, 2, 3, 4, 5, 6}, 9, []int{-1, -1}},
+		{[]int{1, 2, 3, 4, 5, 6}, -1, []int{-1, -1}},
+		{[]int{4, 5, 6}, 1, []int{-1, -1}},
+		{[]int{}, 3, []int{-1, -1}},
+		{[]int{1, 1, 1, 1}, 1, []int{0, 3}},
+	}
+
+	for _, tt := range testsDouble {
+		testname := fmt.Sprintf("Double: %v, %d", tt.inArray, tt.inTarget)
+		t.Run(testname, func(t *testing.T) {
+			ans := DoubleBinarySearch(tt.inArray, tt.inTarget)
 			if !compSlice(ans, tt.want) {
 				t.Errorf("got %v, want %v", ans, tt.want)
 			}
